317d4a1241e3f0278e20895df14487562354dc3d
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2022-11-05 16:42:55 -0500

    ADD: Agregando clase Authority Doamin

---
+++ ventas-backend/ventas/src/main/java/co/edu/poli/ventas/domain/Authority.java
@@ -0,0 +1,41 @@
+package co.edu.poli.ventas.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Authority implements Serializable {
+
+    @Id
+    private String name;
+
+    public Authority() {
+    }
+
+    public Authority(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Authority authority = (Authority) o;
+        return Objects.equals(name, authority.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
