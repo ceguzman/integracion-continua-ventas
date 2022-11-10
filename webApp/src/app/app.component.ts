0ad08a36776276889ad526c2f4eee73c7ceae7f6
Faber Ordoñez <faberordonez1@gmail.com>
2022-11-09 23:58:30 -0500

    Se agrega linter & service para enviar peticiones

--- webApp/src/app/app.component.ts
+++ webApp/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { BaseService } from './core/services/base.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'webApp';
+export class AppComponent implements OnInit,OnDestroy {
+
+  unsubscribe$ = new Subject<void>()
+  productos=[]
+
+  constructor(private baseService:BaseService){}
+
+  ngOnInit(): void {
+    // this.cargarParametros()
+  }
+
+  ngOnDestroy(): void {
+   this.unsubscribe$.next()
+   this.unsubscribe$.complete()    
+  }
+
+  cargarParametros(){
+    this.baseService.obtenerParametros()
+    .pipe(takeUntil(this.unsubscribe$))
+    .subscribe(
+      (response)=>{console.log(response)
+      
+      this.productos = response},
+      (error)=>{console.log(error)})
+  }
 }
---
+++ webApp/src/app/core/services/base.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable } from 'rxjs';
+import {HttpClient} from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+
+  constructor(private http:HttpClient) { }
+
+  obtenerParametros(parametros?:any):Observable<any>{
+    return this.http.get('api/v1/civilizations',{
+      // params:{...parametros},
+    // headers:headers,
+  }).pipe(map(response => response),
+     catchError(error => error)
+  )  
+  }
+
+  actualizar(body:any):Observable<any>{
+    return this.http.post('urlApi',body,{
+    // headers:headers,
+  }).pipe(map(response => response),
+     catchError(error => error)
+  )  
+  }
+}
