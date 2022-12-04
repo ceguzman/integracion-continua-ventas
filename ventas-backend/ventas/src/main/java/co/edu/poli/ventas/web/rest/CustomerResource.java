1739851c9568c84c736e8982aa1b2036db44dd22
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2022-12-04 18:18:36 -0500

    MODIFY: agregando comentarios de CustomerResource

--- ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/CustomerResource.java
+++ ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/CustomerResource.java
@@ -18,18 +18,34 @@ public class CustomerResource {
     @Autowired
     CustomerRepository customerRepository;
 
+    /**
+     * {@code GET  /customers} : get all the customers.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of customers in body.
+     */
     @GetMapping("/customer")
     public ResponseEntity<List<Customer>> findAll(){
         List<Customer> query =(List<Customer>) customerRepository.findAll();
         return  ResponseEntity.ok().body(query);
     }
 
+    /**
+     * {@code GET  /customers/:id} : get the "documentNumber" customer.
+     *
+     * @param documentNumber the id of the customer to retrieve.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the customerDTO, or with status {@code 404 (Not Found)}.
+     */
     @GetMapping("/customer/DocumentNumber/{documentNumber}")
     public ResponseEntity<Customer> findByDocumentNumber(@PathVariable String documentNumber) {
         Customer query = customerRepository.findByDocumentNumber(documentNumber);
         return ResponseEntity.ok().body(query);
     }
 
+    /**
+     * Find by like
+     * @param like the id of the customer
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the customerDTO, or with status {@code 404 (Not Found)}.
+     */
     @GetMapping("/customer/number-Like")
     public ResponseEntity<List<Customer>> findByDocumentNumberLike(@RequestParam(name = "like") String like) {
         List<Customer> query =customerRepository.findByDocumentNumberLike(like);
@@ -37,13 +53,20 @@ public class CustomerResource {
     }
 
     @GetMapping("/customer/count")
-    public ResponseEntity getfindCound(){
+    public ResponseEntity getFindCount(){
         Long query = customerRepository.count();
         return ResponseEntity.ok().body(query);
     }
 
+    /**
+     * {@code POST  /customers} : Create a new customer.
+     *
+     * @param customer the customerDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new customerDTO, or with status {@code 400 (Bad Request)} if the customer has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
     @PostMapping("/customer")
-    public ResponseEntity<Customer> createDependency(@RequestBody Customer customer) throws URISyntaxException {
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) throws URISyntaxException {
         if (customer.getId() != null) {
             return ResponseEntity.badRequest().build();
         }else {
@@ -53,6 +76,15 @@ public class CustomerResource {
         }
     }
 
+    /**
+     * {@code PUT  /customers} : Updates an existing customer.
+     *
+     * @param customer the customerDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated customerDTO,
+     * or with status {@code 400 (Bad Request)} if the customerDTO is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the customerDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
     @PutMapping("/customer")
     public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer){
         if (customer.getId() == null) {
@@ -63,12 +95,23 @@ public class CustomerResource {
                 .body(result);
     }
 
+    /**
+     * {@code DELETE  /customers/:id} : delete the "id" customer.
+     *
+     * @param documentNumber the id of the customer to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/customer/{documentNumber}")
     public ResponseEntity<Customer> deleteByDocumentNumber(@PathVariable String documentNumber) {
         customerRepository.deleteByDocumentNumber(documentNumber);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * {@code DELETE  /customers} : delete all the customer.
+     *
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/customer/deleteAll")
     public ResponseEntity<List<Customer>>deleteAll(){
         List<Customer> query =(List<Customer>) customerRepository.findAll();
