7ca61b48b25d541a5af9b6c7f05cea266f5c0b8f
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2022-12-04 17:58:40 -0500

    MODIFY: agregand comentarios a UserResourse

--- ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/UserResource.java
+++ ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/UserResource.java
@@ -18,12 +18,23 @@ public class UserResource {
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * {@code GET /users} : get all users.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all users.
+     */
     @GetMapping("/user")
     public ResponseEntity<List<User>> findAll() {
         List<User> user = (List<User>) userRepository.findAll();
         return ResponseEntity.ok().body(user);
     }
 
+    /**
+     * {@code GET /users/:login} : get the "login" user.
+     *
+     * @param login the login of the user to find.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the "login" user, or with status {@code 404 (Not Found)}.
+     */
     @GetMapping("/user/userlogin/{login}")
     public ResponseEntity<User> findByLogin(@PathVariable String login) {
         User user = userRepository.findByLogin("carluchin");
@@ -36,6 +47,17 @@ public class UserResource {
         return ResponseEntity.ok().body(user);
     }
 
+    /**
+     * {@code POST  /users}  : Creates a new user.
+     * <p>
+     * Creates a new user if the login and email are not already used, and sends an
+     * mail with an activation link.
+     * The user needs to be activated on creation.
+     *
+     * @param user the user to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new user, or with status {@code 400 (Bad Request)} if the login or email is already in use.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
     @PostMapping("/user")
     public ResponseEntity<User> insert(@RequestBody User user) throws URISyntaxException {
         if (user.getId() != null) {
@@ -47,6 +69,12 @@ public class UserResource {
         }
     }
 
+    /**
+     * {@code PUT /users} : Updates an existing User.
+     *
+     * @param user the user to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated user.
+     */
     @PutMapping("/user")
     public ResponseEntity<User> update(@RequestBody User user) {
         if (user.getId() == null) {
@@ -57,6 +85,12 @@ public class UserResource {
                 .body(result);
     }
 
+    /**
+     * {@code DELETE /users/:login} : delete the "login" User.
+     *
+     * @param login the login of the user to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/user/{login}")
     public ResponseEntity<User> delete(@PathVariable String login) {
         userRepository.deleteByLogin(login);
@@ -69,6 +103,11 @@ public class UserResource {
         return ResponseEntity.ok().body(user);
     }
 
+    /**
+     * {@code DELETE /users/:login} : delete all Users.
+     *
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/user/deleteAll")
     public ResponseEntity<Authority> deleteAll() {
         List<User> query = (List<User>) userRepository.findAll();
