c4a7d41a7485ee82eb9f618fc42b7c8e02ab478f
Carlos Esteban Guzman Baquero <ceguzman582@misena.edu.co>
2022-12-04 17:12:15 -0500

    MODIFY: agregando comentarios en TypeDocumentResource

--- ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/TypeDocumentResource.java
+++ ventas-backend/ventas/src/main/java/co/edu/poli/ventas/web/rest/TypeDocumentResource.java
@@ -17,6 +17,11 @@ public class TypeDocumentResource {
     @Autowired
     TypeDocumentRepository typeDocumentRepository;
 
+    /**
+     * {@code GET  /type-documents} : get all the typeDocuments.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of typeDocuments in body.
+     */
     @GetMapping("/typedocument")
     public ResponseEntity<List<TypeDocument>> findAll() {
         List<TypeDocument> typeDocumentList = (List<TypeDocument>) typeDocumentRepository.findAll();
@@ -35,6 +40,13 @@ public class TypeDocumentResource {
         return ResponseEntity.ok().body(typeDocumentList);
     }
 
+    /**
+     * {@code POST  /type-documents} : Create a new typeDocument.
+     *
+     * @param typeDocument the typeDocumentDTO to create.
+     * @return the {@link ResponseEntity} with status {@code 201 (Created)} and with body the new typeDocumentDTO, or with status {@code 400 (Bad Request)} if the typeDocument has already an ID.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
     @PostMapping("/typedocument")
     public ResponseEntity<TypeDocument> insert(@RequestBody TypeDocument typeDocument) throws URISyntaxException {
         if (typeDocument.getId() != null) {
@@ -46,6 +58,15 @@ public class TypeDocumentResource {
         }
     }
 
+    /**
+     * {@code PUT  /type-documents} : Updates an existing typeDocument.
+     *
+     * @param typeDocument the typeDocumentDTO to update.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body the updated typeDocumentDTO,
+     * or with status {@code 400 (Bad Request)} if the typeDocumentDTO is not valid,
+     * or with status {@code 500 (Internal Server Error)} if the typeDocumentDTO couldn't be updated.
+     * @throws URISyntaxException if the Location URI syntax is incorrect.
+     */
     @PutMapping("/typedocument")
     public ResponseEntity<TypeDocument> update(@RequestBody TypeDocument typeDocument) {
         if (typeDocument.getId() == null) {
@@ -56,18 +77,33 @@ public class TypeDocumentResource {
                 .body(result);
     }
 
+    /**
+     * {@code DELETE  /type-documents/:id} : delete the "id" typeDocument.
+     *
+     * @param nameDocument the id of the typeDocumentDTO to delete.
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/typedocument/{nameDocument}")
     public ResponseEntity<TypeDocument> delete(@PathVariable String nameDocument) {
         typeDocumentRepository.deleteByNameDocument(nameDocument);
         return ResponseEntity.ok().build();
     }
 
+    /**
+     * count
+     * @return number
+     */
     @GetMapping("/typedocument/count")
     public ResponseEntity findCount() {
         Long typeDocument = typeDocumentRepository.count();
         return ResponseEntity.ok().body(typeDocument);
     }
 
+    /**
+     * {@code DELETE  /type-documents} : delete all typeDocument.
+     *
+     * @return the {@link ResponseEntity} with status {@code 204 (NO_CONTENT)}.
+     */
     @DeleteMapping("/typedocument/deleteAll")
     public ResponseEntity<TypeDocument> deleteAll() {
         List<TypeDocument> query = (List<TypeDocument>) typeDocumentRepository.findAll();
